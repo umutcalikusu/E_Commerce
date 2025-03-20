52c7f073996554e8aee02e1212fa2fded2919985
umutcalikusu <umut54709@gmail.com>
2025-03-21 00:22:11 +0300

    E_Commerce Section 2

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(StoreContext context) : ControllerBase
+    {
+        [HttpGet]
+
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await context.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            return Ok(product);
+        }
+    }
+}
---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,10 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public required DbSet<Product> Products { get; set; }
+    }
+}
