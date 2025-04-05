87432ac53e11f89bf902dead1dd478c24eb99dde
umutcalikusu <umut54709@gmail.com>
2025-04-05 19:57:09 +0300

    End of Section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import { Product } from "../models/product";
-import Catalog from "../../features/catalog/Catalog";
+import { useState } from "react";
 import {
   Box,
   Container,
@@ -9,9 +7,9 @@ import {
   ThemeProvider,
 } from "@mui/material";
 import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
   const [darkMode, setDarkMode] = useState(false);
   const palleteType = darkMode ? "dark" : "light";
   const theme = createTheme({
@@ -25,11 +23,6 @@ function App() {
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
-  useEffect(() => {
-    fetch("https://localhost:5001/api/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
-  }, []);
 
   return (
     <ThemeProvider theme={theme}>
@@ -45,7 +38,7 @@ function App() {
         }}
       >
         <Container maxWidth="xl" sx={{ mt: 8 }}>
-          <Catalog products={products} />
+          <Outlet />
         </Container>
       </Box>
     </ThemeProvider>
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,11 +1,16 @@
 import { Container } from "@mui/material";
 import ProductList from "./ProductList";
 import { Product } from "../../app/models/product";
+import { useEffect, useState } from "react";
 
-type Props = {
-  products: Product[];
-};
-export default function Catalog({ products }: Props) {
+export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <Container>
       <ProductList products={products} />
